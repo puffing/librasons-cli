e1f71ead903e782be92c0d7070771a0e70259854
puffing <819051266@qq.com>
2020-07-13 15:26:59 +0800

    删除旧代码

--- bin/config/index.js
+++ bin/config/index.js
@@ -1,20 +1,20 @@
 /*
- @dest: 使用配置文件
- @Author: tree
+  @dest: 使用配置文件
+  @Author: tree
  */
 module.exports  = {
-    npmUrl: 'https://registry.npmjs.org/shc-cli/latest',
-    promptTypeList:[{
-        type: 'list',
-        message: '请选择拉取的模版类型:',
-        name: 'type',
-        choices: [{
-            name: 'pc',
-            value: {
-                url: 'https://github.com/littleTreeme/vue-web-template.git',
-                gitName: 'vue-web-template',
-                val:'PC端模版'
-            }
-        }]
-    }],
+  npmUrl: 'https://registry.npmjs.org/shc-cli/latest',
+  promptTypeList:[{
+      type: 'list',
+      message: '请选择拉取的模版类型:',
+      name: 'type',
+      choices: [{
+        name: 'pc',
+        value: {
+          url: 'https://github.com/puffing/vue-libra-cli-template.git',
+          gitName: 'vue-libra-cli-template',
+          val:'PC端模版'
+        }
+      }]
+  }],
 };
